3cb81214a361ea29e490396089acaca4c195a183
thomast8067 <thomast8067@student.faytechcc.edu>
2025-08-27 14:43:23 +0000

    Created M1Lab and added initial code

--- m1lab.cpp
+++ m1lab.cpp
@@ -16,9 +16,13 @@ int main() {
     int    item_count = 10;
     double item_price = 832.17;
 
+    // give sales pitch
     cout << "Welcome to the " << item_name << " store!" << endl;
+    cout << "Each " << item_name << " is $" << item_price << " per dose." << endl;
+
     // do the processing
     double total_cost = item_count * item_price;
+    
     // end program
     return 0;
 }
