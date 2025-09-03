711f4613aa77bb1ad32916203ff6dde82ee01add
thomast8067 <thomast8067@student.faytechcc.edu>
2025-09-03 14:48:16 +0000

    Added user input

--- m2t1.cpp
+++ m2t1.cpp
@@ -3,7 +3,7 @@ CSC 134
 M2T1 - Customizable sales program
 T Thomas
 9/3/25
-For the entrepreneur in you
+For the entrepreneur in you.
 */
 
 #include <iostream>
@@ -11,22 +11,34 @@ using namespace std;
 
 int main() {
     // ---start program---
-    // declare all of our variables
+    // declare static variables
     string item_name  = "insulin";
     int    item_count = 10;
     double item_price = 832.17;
+
+    // variables for user input
+    int purchase_count;
+    int purchase_price;
 
     // give sales pitch
     cout << "Welcome to the " << item_name << " store!" << endl;
     cout << "Each " << item_name << " is $" << item_price << " per dose." << endl;
     cout << "We have " << item_count << " doses for sale." << endl;
+    cout << endl;
+    cout << "How many would you like to buy?" << endl;
 
-    // two plus two is four minus one thats three quick maths
+    // user input
+    cin >> purchase_count;
+
+    // processing
     double total_cost = item_count * item_price;
 
-    // output quick maths
+    // output processing
+    cout << "You are buying " << purchase_count << " " << item_name << "." << endl;
+/*
     cout << "Buying all of the doses will cost $" << total_cost << endl;
     cout << "Thanks for shopping with us!" << endl;
+*/
 
     // ---end program---
     return 0;
