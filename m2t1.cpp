f18bc187a03e1183e658bc4254dedc948a8136b0
thomast8067 <thomast8067@student.faytechcc.edu>
2025-09-03 14:31:37 +0000

    Copypasting from M1Lab

---
+++ m2t1.cpp
@@ -0,0 +1,33 @@
+/*
+CSC 134
+M2T1 - Customizable sales program
+T Thomas
+9/3/25
+For the entrepreneur in you
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // ---start program---
+    // declare all of our variables
+    string item_name  = "insulin";
+    int    item_count = 10;
+    double item_price = 832.17;
+
+    // give sales pitch
+    cout << "Welcome to the " << item_name << " store!" << endl;
+    cout << "Each " << item_name << " is $" << item_price << " per dose." << endl;
+    cout << "We have " << item_count << " doses for sale." << endl;
+
+    // two plus two is four minus one thats three quick maths
+    double total_cost = item_count * item_price;
+
+    // output quick maths
+    cout << "Buying all of the doses will cost $" << total_cost << endl;
+    cout << "Thanks for shopping with us!" << endl;
+
+    // ---end program---
+    return 0;
+}
