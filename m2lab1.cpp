1f431bcc89a03d885a0df926e0f3f652dc0b0283
thomast8067 <thomast8067@student.faytechcc.edu>
2025-09-08 14:34:34 +0000

    added pseudocode and variables

--- m2lab1.cpp
+++ m2lab1.cpp
@@ -9,3 +9,24 @@ Who makes all the crates for the sokoban puzzles?
 #include <iostream>
 using namespace std;
 
+int main(){
+
+    double length, width, height;
+    double production_cost, customer_cost;
+    double profit;
+
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = .5;
+
+    // Ask the user to input the crate’s length.
+// Ask the user to input the crate’s width.
+// Ask the user to input the crate’s height.
+// Calculate the crate’s volume.
+// Calculate the cost of building the crate.
+// Calculate the customer’s charge for the crate.
+// Calculate the profit made from the crate.
+// Display the crate’s volume.
+// Display the cost of building the crate.
+// Display the customer’s charge for the crate.
+// Display the profit made from the crate.
+}
