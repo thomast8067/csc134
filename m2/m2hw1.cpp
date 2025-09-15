d2f06a2eba144647bbabfcb0c2c21c33af412eca
thomast8067 <thomast8067@student.faytechcc.edu>
2025-09-15 14:42:34 +0000

    Adding base structure

---
+++ m2/m2hw1.cpp
@@ -0,0 +1,36 @@
+/*
+CSC 134
+M2HW1 - Four Parter
+T Thomas
+9/15/25
+GO FOR THE GOLD NUMERO UNO LETS GOOOOOOOOOOOOO
+*/
+
+#include <iostream>
+using namespace std;
+
+void question1() {
+    cout << "Question 1" << endl;
+}
+
+void question2() {
+    cout << "Question 2" << endl;
+}
+
+void question3() {
+    cout << "Question 3" << endl;
+}
+
+void question4() {
+    cout << "Question 4" << endl;
+}
+
+int main(){
+    // call each question as its own function
+
+    question1();
+    question2();
+    question3();
+    question4();
+    return 0;
+}
