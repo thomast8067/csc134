e5759982242561942d8a7d8d9ed9b2399425372c
thomast8067 <thomast8067@student.faytechcc.edu>
2025-09-15 15:11:02 +0000

    Made the receipt look more like a receipt, also used setprecision instead of weird rounding bs

--- m2/m2t2.cpp
+++ m2/m2t2.cpp
@@ -7,26 +7,29 @@ For the entrepreneur in you.
 */
 
 #include <iostream>
-#include <cmath>
+#include <iomanip>
 using namespace std;
 
 int main() {
     // ---start program---
     // declare static variables
-    string item_name  = "sandwich";
+    string item_name  = "SANDWICH";
     double item_price = 5.99;
     double tax_rate   = 0.08;
 
     // start receipt
-    cout << "The price of the " << item_name << " is $" << item_price << "." << endl;
+    cout << "Order Number 413" << endl;
+    cout << item_name << " (x1)" << "\t$" << item_price << endl;
+    cout << "----------------------" << endl;
 
     // processing
-    double tax_price = round(item_price * tax_rate * 100) / 100;
+    cout << setprecision(2) << fixed;
+    double tax_price = item_price * tax_rate; 
     double total_cost = tax_price + item_price;
 
     // output processing
-    cout << "The tax is $" << tax_price << "." << endl;
-    cout << "The total cost is $" << total_cost << "." << endl;
+    cout << "TAX" << "\t\t$"<< tax_price << endl;
+    cout << "TOTAL" << "\t\t$" << total_cost << endl;
     cout << "Thank you for shopping with us!" << endl;
 
     // ---end program---
