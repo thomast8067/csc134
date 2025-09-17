47d599313e878f2e5cc8db0086d32e842b61ba7a
thomast8067 <thomast8067@student.faytechcc.edu>
2025-09-17 14:43:07 +0000

    Asks users for dimensions of rectangles

--- m3/m3t1.cpp
+++ m3/m3t1.cpp
@@ -10,9 +10,26 @@ It's not a contest.
 using namespace std;
 
 int main() {
+    double length1, width1, length2, width2;
+    cout << "Enter the length and width of the rectangles." << endl;
+    cout << "First rectangle." << endl;
+    cout << "Length: ";
+    cin >> length1;
+    cout << "Width: ";
+    cin >> width1;
+    cout << endl;
+    cout << "Second rectangle." << endl;
+    cout << "Length: ";
+    cin >> length2;
+    cout << "Width: ";
+    cin >> width2;
+    cout << endl;
 
+    double area1 = length1 * width2;
+    double area2 = length2 * width2;
 
-
+    cout << "The area of the first rectangle is: " << area1 << endl;
+    cout << "The area of the second rectangle is: " << area2 << endl;
 
     return 0;
 }
