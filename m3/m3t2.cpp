8103addc73e880c9b806f13952544745ecc0d7a3
thomast8067 <thomast8067@student.faytechcc.edu>
2025-09-22 14:44:54 +0000

    Added case for 7 rolled

--- m3/m3t2.cpp
+++ m3/m3t2.cpp
@@ -17,6 +17,28 @@ int roll();
 int main() {
     int num = roll();
     cout << roll() << endl;
+    // Main cycle
+    /*
+    roll 2d6
+    branch if win, lose, or points
+    rest comes later
+    */
+    int roll1 = 2;
+    int roll2 = 5;
+
+    int sum = roll1 + roll2;
+
+    cout << "Roll 1: " << roll1 << endl;
+    cout << "Roll 2: " << roll2 << endl;
+    cout << "Total: " << sum << endl;
+
+    if (sum==7) {
+        cout << "Lucky seven -- You win!" << endl;
+    }
+    else {
+        cout << "Did not roll a seven." << endl;
+    }
+
     return 0;
 }
 
