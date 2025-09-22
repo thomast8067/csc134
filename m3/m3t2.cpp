ebc008b856acac9cb9ebb9abe0c9b9424d53280d
thomast8067 <thomast8067@student.faytechcc.edu>
2025-09-22 14:58:02 +0000

    added point

--- m3/m3t2.cpp
+++ m3/m3t2.cpp
@@ -17,15 +17,22 @@ int roll();
 int main() {
     int num = roll();
     cout << roll() << endl;
+   
     // Main cycle
     /*
     roll 2d6
     branch if win, lose, or points
     rest comes later
     */
-    int roll1 = 2;
-    int roll2 = 5;
 
+    int roll1;
+    int roll2;
+    int point;  // roll if it doesn't immediately win/lose
+
+    cout << "Enter two dice (press ENTER between)";
+    cin >> roll1;
+    cin >> roll2;
+    
     int sum = roll1 + roll2;
 
     cout << "Roll 1: " << roll1 << endl;
@@ -36,7 +43,9 @@ int main() {
         cout << "Lucky seven -- You win!" << endl;
     }
     else {
+        point = sum;
         cout << "Did not roll a seven." << endl;
+        cout << "Your point is: " << point << endl;
     }
 
     return 0;
