e329ae9a58e8f77a3994aab1a1bb5eccde9dc54d
thomast8067 <thomast8067@student.faytechcc.edu>
2025-09-24 14:50:05 +0000

    Starting the lab

---
+++ m3/m3lab1.cpp
@@ -0,0 +1,35 @@
+/*
+CSC 134
+M3T1 - CYOA
+T Thomas
+9/24/25
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void prison(); //start
+
+
+int main() {
+    cout << "==================" << endl;
+    cout << "   PRISON BREAK   " << endl;
+    cout << "==================" << endl;
+    cout << endl;
+
+    cout << "You are in prison, sitting on your very bad mattress counting the bricks in the wall. \nBut, in the back of your head you're plotting your epic escape that's due to happen any second now... now what was the first step again?" << endl;
+    prison();
+
+    cout << "=============" << endl;
+    cout << "   THE END   " << endl;
+    cout << "=============" << endl;
+    cout << endl;
+    
+    return 0;
+}
+
+void prison() {
+    
+
+}
