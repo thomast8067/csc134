c1d0dc76fdfc89e0fdb4d4ad0b282dfe02d9882b
thomast8067 <thomast8067@student.faytechcc.edu>
2025-09-24 15:41:44 +0000

    added cake dialogue need to add options

--- m3/m3lab1.cpp
+++ m3/m3lab1.cpp
@@ -17,7 +17,7 @@ void prison_wait();
 void prison_breakbars();
 void prison_package();
 
-void cake_gun();
+void cake_bombs();
 void cake_phone();
 void cake_key();
 
@@ -30,8 +30,11 @@ int main() {
 
     cout << "You are in prison, sitting on your very bad mattress counting the bricks in the wall." << endl;
     cout << "But, in the back of your head you're plotting your epic escape that's due to happen any second now... now what was the first step again?" << endl;
+    
     prison();
     
+    cout << "GAME OVER" << endl;
+
     return 0;
 }
 
@@ -83,7 +86,7 @@ void prison_package() {
     cout << "Lucky for you, they don't know you had a friend on the outside sneak something into the pastry." << endl;
 
     cout << "Select an option with 1-3" << endl;
-    cout << "1: Take out the gun" << endl;
+    cout << "1: Take out the bombs" << endl;
     cout << "2: Take out the phone" << endl;
     cout << "3: Take out the skeleton key" << endl;
     
@@ -92,7 +95,7 @@ void prison_package() {
     cout << endl;
 
     if (decision == 1) {
-        cake_gun();
+        cake_bombs();
     }
     else if (decision == 2) {
         cake_phone();
@@ -102,22 +105,24 @@ void prison_package() {
     }
 }
 
-void cake_gun() {
-    cout << "gun" << endl;
-    sleep(2);
-    failure();
+void cake_bombs() {
+    cout << "You pull a bomb out of the cake, finally some real power!" << endl;
+    cout << "Where should you place it?" << endl;
+    
 }
 
 void cake_phone() {
-    cout << "phone" << endl;
+    cout << "You pull the phone out of the cake... oh wait it's dead." << endl;
+    cout << "And they didn't put a charger in here either." << endl;
+    cout << "Great." << endl;
     sleep(2);
     failure();
 }
 
 void cake_key() {
-    cout << "key" << endl;
-    sleep(2);
-    failure();
+    cout << "You pull the skeleton key out of the cake." << endl;
+    cout << "You wait until lights out then you make your move, reaching around the bars and opening the cell." << endl;
+    cout << "With your newfound freedom, where do you go now?" << endl;
 }
 
 void failure() {
