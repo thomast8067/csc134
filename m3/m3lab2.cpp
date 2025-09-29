6f828b4767360035df649f1a1e5aa2c8ee02531c
thomast8067 <thomast8067@student.faytechcc.edu>
2025-09-29 14:37:35 +0000

    added skeleton of letter grader

--- m3/m3lab2.cpp
+++ m3/m3lab2.cpp
@@ -16,5 +16,22 @@ void letter_grader();
 void combat();
 
 int main() {
+
+    letter_grader();
+
     return 0;
+}
+
+void letter_grader() {
+
+    double number_grade;
+    string letter_grade;
+
+    cout << "Enter a number grade from 0-100:" << endl;
+    cin >> number_grade;
+
+    letter_grade = "A";
+
+    cout << "A number grade of " << number_grade << " is a(n) " << letter_grade << "." << endl;
+
 }
