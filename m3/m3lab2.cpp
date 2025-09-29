02a83b470f5a4eaf05292372db6f17e0cfeac1d3
thomast8067 <thomast8067@student.faytechcc.edu>
2025-09-29 14:31:27 +0000

    Started lab 2

---
+++ m3/m3lab2.cpp
@@ -0,0 +1,20 @@
+/*
+CSC 134
+M3LAB2 - Letter Grades
+T Thomas
+9/29/25
+
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void letter_grader();
+void combat();
+
+int main() {
+    return 0;
+}
