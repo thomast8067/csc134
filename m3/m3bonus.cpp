777eb19ed42a85f16ee8b117a83510346af54212
thomast8067 <thomast8067@student.faytechcc.edu>
2025-10-01 15:05:14 +0000

    added probabilites to pigs

--- m3/m3bonus.cpp
+++ m3/m3bonus.cpp
@@ -12,6 +12,7 @@ Making bacon.
 
 using namespace std;
 
+
 int roll();
 
 int main() {
@@ -24,6 +25,16 @@ int main() {
     pig1 = roll();
     pig2 = roll();
 
+    cout << pig1 << "\n" << pig2 << endl;
+    
+    return 0;
+}
+
+int roll() {
+
+    int pig_roll;
+    pig_roll = rand() % 1000 + 1;
+
     /*
     assigning values to the numbers based off probabilties
     Position        Percentage
@@ -35,14 +46,40 @@ int main() {
     Leaning Jowler  0.6%
     */
 
+    const int SIDE_NO_DOT = 1000;
+    const int SIDE_DOT = 605;
+    const int RAZORBACK = 348;
+    const int TROTTER = 124;
+    const int SNOUTER = 36;
+    const int LEANING_JOWLER = 6;
 
-    cout << pig1 << "\n" << pig2 << endl;
-    
-    return 0;
-}
-
-int roll() {
     int pig_value;
-    pig_value = rand() % 1000 + 1;
+
+    // all pig values are prime numbers so we can leverage the fundamental theorem of arithmetic later
+
+    if (pig_roll <= LEANING_JOWLER){
+        pig_value = 13;
+    }
+
+    else if (pig_roll <= SNOUTER){
+        pig_value = 11;
+    }
+
+    else if (pig_roll <= TROTTER){
+        pig_value = 7;
+    }
+
+    else if (pig_roll <= RAZORBACK){
+        pig_value = 5;
+    }
+
+    else if (pig_roll <= SIDE_DOT){
+        pig_value = 3;
+    }
+
+    else if (pig_roll <= SIDE_NO_DOT){
+        pig_value = 2;
+    }
+
     return pig_value;
 }
