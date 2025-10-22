56db5b531cdff9bdfc8075502ae8fd2cac33c1f5
thomast8067 <thomast8067@student.faytechcc.edu>
2025-10-22 14:54:13 +0000

    added say_hello

--- m5/m5t1.cpp
+++ m5/m5t1.cpp
@@ -14,18 +14,36 @@ void say_hello();
 
 int get_the_answer();
 
-double double_the_number();
+double double_the_number(double);
 
 int main() {
     // This program does nothing!
     double my_num;
     int another_num;
 
-    cout << "Welcome to a very cool program indeed!" << endl;
+    say_hello();
     cout << "Please enter a number (with or without a decimal point.)" << endl;
     cin >> my_num;
-    my_num = my_num * 2;
+    //my_num = my_num * 2;
+    my_num = double_the_number(my_num);
     cout << "Double the number is: " << my_num << endl;
-    
+
     return 0;
+}
+
+// Function definitions
+
+void say_hello() {
+    // says hi
+    cout << "Welcome to a very cool program indeed!" << endl;
+}
+
+int get_the_answer(){
+    //provides answer
+}
+
+double double_the_number(double the_num){
+    //num times two
+    double answer = the_num * 2;
+    return answer;
 }
