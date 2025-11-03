84580553654332cb4e5aee65a6e09bffb597adc5
thomast8067 <thomast8067@student.faytechcc.edu>
2025-11-03 15:14:41 +0000

    added program from file and design specs

---
+++ m5/m5lab2.cpp
@@ -0,0 +1,59 @@
+/*
+CSC 134
+M5LAB2 - Debug 
+T Thomas
+11/3/25
+*/
+
+#include <iostream>
+using namespace std;
+
+/*
+Here are the program specifications.
+
+The program will ask the user to enter the width and length of a rectangle. It will then calculate the area. Finally it will display all three values in well formatted output.
+
+The program will contain the following functions as well as main().
+
+getLength - Asks the user to enter a rectangle's length, and return that value as a double.
+
+getWidth - Asks the user to enter a rectangle's width, and return that value as a double.
+
+getArea - This function should take two arguments, length and width. It will calculate the area and return that value as a double.
+
+displayData - This function should take three arguments, length, width, and area. It will then display these values as well formatted output. ("Well formatted" is subjective, at minimum each value should be clearly labeled. The Apple Orchard program from Module One is an example of the type of output format you might use.)
+*/
+
+// Declare (Write the prototypes for)
+// the getLength,
+// getWidth, getArea, and displayData
+// functions here.
+
+int main()
+{
+	// This program calculates the area of a rectangle.
+	// TODO: fix any syntax errors
+	
+   double length,    // The rectangle's length
+          width,     // The rectangle's width
+          area;      // The rectangle's area
+          
+   // Get the rectangle's length.
+   length = getLength();
+   
+   // Get the rectangle's width.
+   width = getWidth()
+   
+   // Get the rectangle's area.
+   are = getArea();
+   
+   // Display the rectangle's data.
+   displayData(length, width, area);
+          
+   return 0;
+}
+
+//***************************************************
+// TODO: write the getLength, getWidth, getArea,    *
+// and displayData functions below.                 *
+//***************************************************
