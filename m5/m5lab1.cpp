64600262baf346cfe2462bdbbce140d65d718369
thomast8067 <thomast8067@student.faytechcc.edu>
2025-11-05 15:33:11 +0000

    added basic choice system

---
+++ m5/m5lab1.cpp
@@ -0,0 +1,40 @@
+/*
+CSC 134
+M5LAB1 - CYOA 
+T Thomas
+11/5/25
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int getPlayerChoice(int maxChoice);
+
+
+int main(){
+
+    int choice;
+    int maxChoice = 3;
+
+    cout << "Testing: Choose 1, 2, or 3." << endl;
+    choice = getPlayerChoice(maxChoice);
+    cout << "You chose: " << choice << endl;
+}
+
+
+int getPlayerChoice(int maxChoice){
+    int choice;
+    while(true){
+        cout << "Your choice: ";
+        cin >> choice;
+
+        if(choice >= 1 && choice <= maxChoice){
+            return choice;
+        }
+
+        cout << "Please choose between 1 and " << maxChoice << ".\n";
+    }
+
+}
