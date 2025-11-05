112f8370e3f64de31f87fa33bc64bb2891551acb
thomast8067 <thomast8067@student.faytechcc.edu>
2025-11-05 16:20:52 +0000

    added first choice

--- m5/m5lab1.cpp
+++ m5/m5lab1.cpp
@@ -6,42 +6,52 @@ T Thomas
 */
 
 #include <iostream>
+#include <unistd.h>
 
 using namespace std;
 
 
-int getPlayerChoice(int maxChoice);
+int getPlayerChoice();
 void showChoices(string choice1, string choice2, string choice3);
 
-int main(){
-    int choice;
-    int maxChoice = 3;
+const int MAXCHOICE = 3;
 
-    showChoices("This is choice 1","This is choice 2","This is choice 3");
-    choice = getPlayerChoice(maxChoice);
-    cout << "You chose: " << choice << endl;
+void prison(); //start
+void failure(); //lose condition
+
+void prisonWait();
+void prisonBreakBars();
+void prisonPackage();
+
+void cakeBombs();
+void cakePhone();
+void cakeKey();
+
+int main(){
+    
+    prison();
 
     return 0;
 }
 
-int getPlayerChoice(int maxChoice){
+int getPlayerChoice(){
     int choice;
     while(true){
         cout << "Your choice: ";
         cin >> choice;
 
-        if(choice >= 1 && choice <= maxChoice){
+        if(choice >= 1 && choice <= MAXCHOICE){
             return choice;
         }
 
-        cout << "Please choose between 1 and " << maxChoice << ".\n";
+        cout << "Please choose between 1 and " << MAXCHOICE << ".\n";
     }
 
 }
 
 void showChoices(string choice1, string choice2, string choice3){
     
-    cout << "MAKE YOUR CHOICE" << endl;
+    cout << "-~-~-MAKE YOUR CHOICE-~-~-" << endl;
 
     int num = 1;
     cout << num << ". " << choice1 << endl;
@@ -56,4 +66,53 @@ void showChoices(string choice1, string choice2, string choice3){
         cout << num << ". " << choice3 << endl;
         num++;
     }
+}
+
+void prison() {
+
+   int choice;
+
+   cout << R"(You are in prison, sitting on your very bad mattress
+counting the bricks in the wall. But, in the back of
+your head you're plotting an epic escape that's due
+to happen any second now...
+what was the first step again?
+    )";
+
+    showChoices("Bide your time.",
+                "Break the bars with your brute stregth.",
+                "Get a package delivered.");
+
+    choice = getPlayerChoice();
+
+    if (choice == 1) {
+        prisonWait();
+    }
+    if (choice == 2) {
+        cout << "Headed around back..." << endl;
+    }
+    if (choice == 3) {
+        cout << "Forget it, let's get pizza." << endl;
+    }
+}
+
+void failure() {
+    cout << R"(
+=============
+   FAILURE   
+=============)" << endl;
+}
+
+void prisonWait() {
+    cout << "Ah yes, good things come to those who wait after all!" << endl;
+    sleep(1);
+    cout << "." << endl;
+    sleep(1);
+    cout << "." << endl;
+    sleep(1);
+    cout << "." << endl;
+    sleep(1);
+    cout << "...Okay this is boring, maybe next time choose a better option." << endl;
+    sleep(2);
+    failure();
 }
